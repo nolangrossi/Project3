eb86132eeaa6e8af4161ab37935e9746ae89334c
CerfSoleil <robedj12@gmail.com>
2025-03-27 04:19:11 -0500

    Front end Vite Route

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,19 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    }
+  },
   server: {
-    port: 3000,
-    host: '127.0.0.1',
-    open: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
-        secure: false,
+        secure: false, 
         changeOrigin: true,
       },
+      '/auth': {
+        target: 'http://localhost:3001',
+        secure: false,
+        changeOrigin: true,
+      }
     },
   },
-});
+})
