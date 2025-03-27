75c07061290471a87566f3341c30c2678b7a37d9
CerfSoleil <robedj12@gmail.com>
2025-03-27 04:51:59 -0500

    Setting up GitHub Actions testing

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,19 +1,35 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
   server: {
-    port: 3000,
-    host: '127.0.0.1',
-    open: true,
     proxy: {
       '/api': {
+        target: 'http://localhost:3001',
+        secure: false, 
+        changeOrigin: true,
+      },
+      '/auth': {
         target: 'http://localhost:3001',
         secure: false,
         changeOrigin: true,
       },
     },
+  },
+  test: {
+    coverage: {
+      provider: "v8",
+      reporter: ['text', 'json', 'html'],
+    },
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts', // File to include before running tests
   },
 });
