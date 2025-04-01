dd1da753393a8aa286843484f0af966a3261f3c7
Nolan Grossi <nolangrossi6@gmail.com>
2025-03-31 20:30:56 -0400

    fixed vite.config

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -6,7 +6,6 @@ import Game from '../components/GameComponent';
 const Home = () => {
     return (
         <div className="home">
-            <h1 className="page-title">This is the home page.</h1>
             <Game />
         </div>
     )
