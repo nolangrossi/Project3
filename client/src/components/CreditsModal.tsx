936e06dabcb09393ad287f9a4efb6cb0a937d31d
CerfSoleil <robedj12@gmail.com>
2025-03-31 21:23:16 -0500

    Styles for Credits modal

--- client/src/components/CreditsModal.tsx
+++ client/src/components/CreditsModal.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 // import '../styles/creditsModal.css';
+import '../styles/pixelated.css'
 
 interface CreditsModalProps {
   showModal: boolean;
@@ -33,11 +34,17 @@ const CreditsModal: React.FC<CreditsModalProps> = ({ showModal, setShowCreditsMo
     showModal && (
       <div className="modal" onClick={closeModal}>
         <div className="modal-content pixel-corners-grey">
+        <div className="horizontal-border top-border"></div>
+        <div className="vertical-border left-border"></div>
           <button className="close-btn" onClick={handleClose}>X</button>
           <h2>Credits</h2>
           <p>&copy; {new Date().getFullYear()} Your Company Name. All Rights Reserved.</p>
           <p>Developed by Your Name / Team.</p>
           <p>Icons by XYZ, Images by ABC, Fonts by DEF.</p>
+          <div className="horizontal-line top-line"></div>
+          <div className="horizontal-line bottom-line"></div>
+          <div className="horizontal-border bottom-border"></div>
+          <div className="vertical-border right-border"></div>
         </div>
       </div>
     )
