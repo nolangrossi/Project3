05804cac98334a4ef47081375c7ac915cf666903
Andrew <pelfreya6@gmail.com>
2025-04-01 19:40:52 -0400

    sign up and login working

--- client/src/utils/mutations.tsx
+++ client/src/utils/mutations.tsx
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
   mutation loginUser($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
+    loginUser(email: $email, password: $password) {
       token
       user {
         _id
@@ -14,7 +14,7 @@ export const LOGIN_USER = gql`
 `;
 export const SIGNUP_USER = gql`
   mutation registerUser($username: String!, $email: String!, $password: String!) {
-    signup(username: $username, email: $email, password: $password) {
+    registerUser(username: $username, email: $email, password: $password) {
       token
       user {
         _id
