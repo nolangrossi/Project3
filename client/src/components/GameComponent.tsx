3f7326b8c96127e1077e45f0ebbb131ba3871b9a
CerfSoleil <robedj12@gmail.com>
2025-04-01 21:15:34 -0500

    keyboardNavigation.ts | Moved the handleKeyDown function into its own file

--- client/src/components/GameComponent.tsx
+++ client/src/components/GameComponent.tsx
@@ -4,6 +4,7 @@ import { useQuery } from "@apollo/client";
 // Functions
 import { GETRANDOMPOKEMON } from "../utils/queries";
 import { handleInputChange } from "./functions/inputNavigation";
+import { handleKeyDown } from "./functions/keyboardNavigation";
 
 // Components
 import MenuBox from "./MenuBox";
@@ -85,43 +86,6 @@ const Game: React.FC = () => {
       } else {
         setGameMessage(`Game over! The word was: ${selectedPokemon.name}`);
       }
-    }
-  };
-  
-
-  const handleKeyDown = (
-    e: React.KeyboardEvent<HTMLInputElement>,
-    rowIndex: number,
-    cellIndex: number,
-    rows: string[][],
-    setRows: React.Dispatch<React.SetStateAction<string[][]>>,
-    setActiveIndex: React.Dispatch<React.SetStateAction<number>>
-  ) => {
-    const target = e.target as HTMLInputElement;
-    if (e.key === "ArrowRight" && cellIndex < rows[0].length - 1) {
-      setActiveIndex(cellIndex + 1);
-      setTimeout(() => {
-        document.getElementById(`input-${rowIndex}-${cellIndex + 1}`)?.focus();
-      }, 50);
-    } else if (e.key === "ArrowLeft" && cellIndex > 0) {
-      setActiveIndex(cellIndex - 1);
-      setTimeout(() => {
-        document.getElementById(`input-${rowIndex}-${cellIndex - 1}`)?.focus();
-      }, 50);
-    } else if (e.key === "Backspace" && !target.value) {
-      // Clear current cell and move focus to previous cell
-      const newRows = [...rows];
-      newRows[rowIndex][cellIndex] = ""; // Clear the current cell
-      setRows(newRows);
-  
-      if (cellIndex > 0) {
-        setActiveIndex(cellIndex - 1);
-        setTimeout(() => {
-          document.getElementById(`input-${rowIndex}-${cellIndex - 1}`)?.focus();
-        }, 50);
-      }
-    } else if (e.key === "Enter") {
-      // checkWord();
     }
   };
   
